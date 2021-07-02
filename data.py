ab798a98d0d2d00c8f61f453c17ffb4a62a38bc1
Jaco <58355082+Newke85@users.noreply.github.com>
2021-07-02 07:26:18 +0200

    Added user_input title() start again function
    Added .title() for user input and created one new functtion

--- data.py
+++ data.py
@@ -1,13 +1,13 @@
-types = ['merlot', 'pinotage', 'chenin blanc', 'chardonny']
+types = ['Merlot', 'Pinotage', 'Chenin Blanc', 'Chardonny']
 
-wines = [['merlot', 'De Grendel', 'Durbanville', '2017' ],
-         ['pinotage', 'Beyerskloof', 'Stellenbosch', '2018'],
-         ['chenin blanc', 'Spier', 'Stellenbosch', '2020'],
-         ['chardonny', 'Flag Stone', 'Somerset West', '2019'],
-         ['pinotage', 'Diemersdal', 'Durbanville', '2019'],
-         ['chardonny', 'De Grendel', 'Durbanville', '2020'],
-         ['merlot', 'Flag Stone', 'Somerset West', '2016'],
-         ['chenin blanc', 'Beyerskloof', 'Stellenbosch', '2019']]
+wines = [['Merlot', 'De Grendel', 'Durbanville', '2017' ],
+         ['Pinotage', 'Beyerskloof', 'Stellenbosch', '2018'],
+         ['Chenin Blanc', 'Spier', 'Stellenbosch', '2020'],
+         ['Chardonny', 'Flag Stone', 'Somerset West', '2019'],
+         ['Pinotage', 'Diemersdal', 'Durbanville', '2019'],
+         ['Chardonny', 'De Grendel', 'Durbanville', '2020'],
+         ['Merlot', 'Flag Stone', 'Somerset West', '2016'],
+         ['Chenin Blanc', 'Beyerskloof', 'Stellenbosch', '2019']]
 
 regions = ['Durbanville', 'Stellenbosch', 'Somerset West']
 
--- script_main_test.py
+++ script_main_test.py
@@ -4,21 +4,18 @@ from data import *
 def welcome():
     print("Welcome to the wine selector")
 
-# Code to insert wine types into a LinkedList. The data is in data.py
 def insert_wine_types():
     wine_type_list = LinkedList()
     for wine_type in types:
         wine_type_list.insert_beginning(wine_type)
     return wine_type_list
 
-# Code to insert wine region into a LinkedList. The data is in data.py
 def insert_wine_region():
     region_list = LinkedList()
     for region in regions:
         region_list.insert_beginning(region)
     return region_list
 
-# Code to insert wine data into a LinkedList of Linkedlist. The data is in data.py
 def insert_wine_list():
     wine_list = LinkedList()
     for wine_type in types:
@@ -29,7 +26,6 @@ def insert_wine_list():
         wine_list.insert_beginning(wine_sublist)
     return wine_list
 
-# Code to insert wine data into a LinkedList of Linkedlist. The data is in data.py
 def insert_region_list():
     area_list = LinkedList()
     for region in regions:
@@ -45,9 +41,8 @@ wine_details = insert_wine_list()
 wine_regions = insert_wine_region()
 region_details = insert_region_list()
 
-# Code to ask user how they would like to search for their wine
 def user_choice():
-    how_user_search = input("How would you like to search for your wine? Please type 'v' for variey or 'r' for region: \n").lower()
+    how_user_search = input("How would you like to search for your wine? Please type 'v' for variey or 'r' for region: \n")
     if how_user_search == 'v':
         choose_variety()
     elif how_user_search == 'r':
@@ -56,13 +51,10 @@ def user_choice():
         print("I did not understand your input, please try again")
         return user_choice()
 
-# Code if user want to search by variety
 def choose_variety():
     print("We have the following varieties of wine to choose from: ")
     print(* types, sep=", ")
     user_selection = input("Please type in the variety that you would like to buy\n").title()
-    
-    # Search for user input in data structure
     matching_wine = []
     get_list_head = variety_of_wine.get_head_node()
     while get_list_head is not None:
@@ -70,8 +62,6 @@ def choose_variety():
             matching_wine.append(get_list_head.get_value())
         get_list_head = get_list_head.get_next_node()
     selected_wine = matching_wine[0]
-
-    # Print a list of wine that matches user input
     print(f"We have the following {selected_wine} availible:")
     wine_list_head = wine_details.get_head_node()
     while wine_list_head.get_next_node() is not None:
@@ -85,15 +75,20 @@ def choose_variety():
                 print("_______________________\n")
                 sublist_head = sublist_head.get_next_node()
         wine_list_head = wine_list_head.get_next_node()
-    start_again()
 
-# Code if user want to search by region
+    start_again = ""
+    while start_again != 'y' or 'n': 
+        start_again = input("Would you like to search for other wine? Press y for yes and n for no: ")
+        if start_again == 'y':
+            user_choice()
+        elif start_again == 'n':
+            print("Goodbye")
+            break
+
 def choose_region():
     print("We have wine from the following regions: ")
     print(* regions, sep=", ")
     user_selection = input("Please type in the region that you would like to explore\n").title()
-
-    # Search for user input in data structure
     selected_wine_region = []
     get_list_head = wine_regions.get_head_node()
     while get_list_head is not None:
@@ -101,8 +96,6 @@ def choose_region():
             selected_wine_region.append(get_list_head.get_value())
         get_list_head = get_list_head.get_next_node()
     selected_area = selected_wine_region[0]
-
-    # Print a list of wine that matches user input
     print(f"We have wine from the following wineries in {selected_area}:")
     wine_list_head = region_details.get_head_node()
     while wine_list_head.get_next_node() is not None:
@@ -116,14 +109,10 @@ def choose_region():
                 print("_______________________\n")
                 sublist_head = sublist_head.get_next_node()
         wine_list_head = wine_list_head.get_next_node()
-    start_again()
 
-
-# Code to aske the user if they would like to search again
-def start_again():
     start_again = ""
     while start_again != 'y' or 'n': 
-        start_again = input("Would you like to search for other wine? Press y for yes and n for no: ").lower()
+        start_again = input("Would you like to search for other wine? Press y for yes and n for no: ")
         if start_again == 'y':
             user_choice()
         elif start_again == 'n':
@@ -133,6 +122,10 @@ def start_again():
 
 def start_wine_selector():
     welcome()
+    insert_wine_types()
+    insert_wine_list()
+    insert_wine_region()
+    insert_region_list()
     user_choice()
 
 
